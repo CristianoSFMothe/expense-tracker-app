b227a3d7c5387555a52a695c425847791e1cc619
CristianoSFMothe <cristianodevsystem@gmail.com>
2025-07-21 19:33:46 -0300

    feat: integrate Cloudinary image upload functionality in user service and add axios dependency

--- services/userService.ts
+++ services/userService.ts
@@ -1,12 +1,29 @@
 import { firestore } from "@/config/firebase";
 import { ResponseType, UserDataType } from "@/types";
 import { doc, updateDoc } from "firebase/firestore";
+import { uploadFileToCloudinary } from "./imageService";
 
 export const updateUser = async (
   uuid: string,
   updatedData: UserDataType,
 ): Promise<ResponseType> => {
   try {
+    if (updatedData.image && updatedData?.image.uri) {
+      const imageUploadResponse = await uploadFileToCloudinary(
+        updatedData.image,
+        "users",
+      );
+
+      if (!imageUploadResponse.success) {
+        return {
+          success: false,
+          msg: imageUploadResponse.msg || "Failed to upload image",
+        };
+      }
+
+      updatedData.image = imageUploadResponse.data;
+    }
+
     const userRef = doc(firestore, "users", uuid);
 
     await updateDoc(userRef, updatedData);
