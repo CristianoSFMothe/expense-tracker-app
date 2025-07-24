4db894682d70eb09435133fff00094559d80733b
CristianoSFMothe <cristianodevsystem@gmail.com>
2025-07-24 00:13:51 -0300

    feat: add TransactionModal component for managing transactions; update dependencies

---
+++ app/(modals)/transactionModal.tsx
@@ -0,0 +1,401 @@
+import BackButton from "@/components/BackButton";
+import Button from "@/components/Button";
+import Header from "@/components/Header";
+import ImageUpload from "@/components/ImageUpload";
+import ModalWrapper from "@/components/ModalWrapper";
+import Typo from "@/components/Typo";
+import { expenseCategories, transactionTypes } from "@/constants/data";
+import { colors, radius, spacingX, spacingY } from "@/constants/theme";
+import { useAuth } from "@/contexts/authContext";
+import useFechData from "@/hooks/useFechData";
+import { deleteWallet } from "@/services/walletService";
+import { TransactionType, WalletType } from "@/types";
+import { scale, verticalScale } from "@/utils/styling";
+import { useLocalSearchParams, useRouter } from "expo-router";
+import { orderBy, where } from "firebase/firestore";
+import * as Icons from "phosphor-react-native";
+import React, { useState } from "react";
+import { Alert, ScrollView, StyleSheet, View } from "react-native";
+import { Dropdown } from "react-native-element-dropdown";
+
+const TransactionModal = () => {
+  const [isLoading, setIsLoading] = useState(false);
+  const { user } = useAuth();
+  const router = useRouter();
+
+  const {
+    data: wallets,
+    error: walletError,
+    loading: walletLoading,
+  } = useFechData<WalletType>("wallets", [
+    where("uid", "==", user?.uid),
+    orderBy("created", "desc"),
+  ]);
+
+  const oldTransaction: { name: string; image: string; id: string } =
+    useLocalSearchParams();
+
+  // useEffect(() => {
+  //   if (oldTransaction?.id) {
+  //     setTransaction({
+  //       name: oldTransaction.name,
+  //       image: oldTransaction.image,
+  //     });
+  //   }
+  // }, []);
+
+  const [transaction, setTransaction] = useState<TransactionType>({
+    type: "expense",
+    amount: 0,
+    category: "",
+    date: new Date(),
+    walletId: "",
+    description: "",
+    image: null,
+  });
+
+  const onSubmit = async () => {
+    // let { name, image } = transaction;
+    // if (!name.trim() || !image) {
+    //   Alert.alert("Carteira", "Por favor, preencha todos os campos.");
+    //   return;
+    // }
+    // const data: WalletType = {
+    //   name,
+    //   image,
+    //   uid: user?.uid,
+    // };
+    // if (oldTransaction?.id) data.id = oldTransaction?.id;
+    // setIsLoading(true);
+    // const response = await createOrUpdateWallet(data);
+    // setIsLoading(false);
+    // if (response.success) {
+    //   // TODO: Adicionar um toast como "Carteira criada com sucesso"
+    //   router.back();
+    // } else {
+    //   Alert.alert("Carteira", response.msg || "Erro ao criar carteira");
+    //   // TODO: Adicionar um toast de erro
+    // }
+  };
+
+  const onDelete = async () => {
+    if (!oldTransaction?.id) return;
+
+    setIsLoading(true);
+
+    const response = await deleteWallet(oldTransaction?.id);
+
+    setIsLoading(false);
+
+    if (response.success) {
+      router.back();
+    } else {
+      Alert.alert("Carteira", response.msg || "Erro ao deletar carteira");
+    }
+  };
+
+  const showDeleteAlert = () => {
+    Alert.alert(
+      "Confirmação",
+      "Tem certeza de que deseja fazer isso? \nEsta ação removerá todas as transações relacionadas a esta carteira.",
+      [
+        {
+          text: "Cancelar",
+          onPress: () => console.log("Cancel Pressed"),
+          style: "cancel",
+        },
+        {
+          text: "Confirmar",
+          onPress: () => onDelete(),
+          style: "destructive",
+        },
+      ],
+    );
+  };
+
+  return (
+    <ModalWrapper style={styles.container}>
+      <View style={styles.container}>
+        <Header
+          title={oldTransaction?.id ? "Editar transação" : "Nova transação"}
+          leftIcon={<BackButton />}
+          style={{ marginBottom: spacingY._10 }}
+        />
+
+        {/* Form */}
+        <ScrollView
+          contentContainerStyle={styles.form}
+          showsVerticalScrollIndicator={false}
+        >
+          {/* Transaction type */}
+          <View
+            style={styles.inputContainer}
+            accessible={true}
+            accessibilityLabel="Campo de nome da carteira"
+            // testID="input-name-container"
+          >
+            <Typo color={colors.neutral200}>Tipo de transação</Typo>
+
+            <Dropdown
+              style={styles.dropdownContainer}
+              activeColor={colors.neutral700}
+              placeholderStyle={styles.dropdownPlaceholder}
+              selectedTextStyle={styles.dropdownSelectedText}
+              iconStyle={styles.dropdownIcon}
+              data={transactionTypes}
+              maxHeight={300}
+              labelField="label"
+              valueField={"value"}
+              itemTextStyle={styles.dropdownItemText}
+              itemContainerStyle={styles.dropdownItemContainer}
+              containerStyle={styles.dropdownListContainer}
+              // placeholder="Selecione uma transação"
+              // placeholder={!isFocus ? "Select item" : "..."}
+              value={transaction.type}
+              onChange={(item) => {
+                setTransaction({ ...transaction, type: item.value });
+              }}
+            />
+          </View>
+
+          {/* Wallets */}
+          <View
+            style={styles.inputContainer}
+            accessible={true}
+            accessibilityLabel="Campo de nome da carteira"
+            // testID="input-name-container"
+          >
+            <Typo color={colors.neutral200}>Carteira</Typo>
+
+            <Dropdown
+              style={styles.dropdownContainer}
+              activeColor={colors.neutral700}
+              placeholderStyle={styles.dropdownPlaceholder}
+              selectedTextStyle={styles.dropdownSelectedText}
+              iconStyle={styles.dropdownIcon}
+              data={wallets.map((wallet) => ({
+                label: `${wallet.name} (R$${wallet.amount})`,
+                value: wallet?.id,
+              }))}
+              maxHeight={300}
+              labelField="label"
+              valueField="value"
+              itemTextStyle={styles.dropdownItemText}
+              itemContainerStyle={styles.dropdownItemContainer}
+              containerStyle={styles.dropdownListContainer}
+              placeholder={"Selecione uma carteira"}
+              value={transaction.walletId}
+              onChange={(item) => {
+                setTransaction({ ...transaction, walletId: item.value || "" });
+              }}
+            />
+          </View>
+
+          {/* Expense categories */}
+          {transaction.type === "expense" && (
+            <View
+              style={styles.inputContainer}
+              accessible={true}
+              accessibilityLabel="Campo de nome da carteira"
+              // testID="input-name-container"
+            >
+              <Typo color={colors.neutral200}>Categoria de despesa</Typo>
+
+              <Dropdown
+                style={styles.dropdownContainer}
+                activeColor={colors.neutral700}
+                placeholderStyle={styles.dropdownPlaceholder}
+                selectedTextStyle={styles.dropdownSelectedText}
+                iconStyle={styles.dropdownIcon}
+                data={Object.values(expenseCategories)}
+                maxHeight={300}
+                labelField="label"
+                valueField="value"
+                itemTextStyle={styles.dropdownItemText}
+                itemContainerStyle={styles.dropdownItemContainer}
+                containerStyle={styles.dropdownListContainer}
+                placeholder={"Selecione uma categoria"}
+                value={transaction.category}
+                onChange={(item) => {
+                  setTransaction({
+                    ...transaction,
+                    category: item.value || "",
+                  });
+                }}
+              />
+            </View>
+          )}
+
+          {/* date picker */}
+
+          <View
+            style={styles.inputContainer}
+            accessible={true}
+            // accessibilityLabel="Campo de nome da carteira"
+            testID="input-name-container"
+          >
+            <Typo color={colors.neutral200}>Transação ícone</Typo>
+            {/* Image input */}
+            <ImageUpload
+              file={transaction.image}
+              onClear={() => setTransaction({ ...transaction, image: null })}
+              onSelect={(file) =>
+                setTransaction({ ...transaction, image: file })
+              }
+              placeholder="Upload imagem"
+            />
+          </View>
+        </ScrollView>
+      </View>
+
+      <View style={styles.footer}>
+        {oldTransaction?.id && !isLoading && (
+          <Button
+            onPress={showDeleteAlert}
+            style={{
+              backgroundColor: colors.rose,
+              paddingHorizontal: spacingX._15,
+            }}
+          >
+            <Icons.TrashIcon
+              color={colors.white}
+              size={verticalScale(24)}
+              weight="bold"
+            />
+          </Button>
+        )}
+        <Button
+          onPress={onSubmit}
+          style={{ flex: 1 }}
+          loading={isLoading}
+          accessibilityLabel="Botão Atualizar"
+          testID="submit-button"
+        >
+          <Typo color={colors.black} fontWeight={"700"}>
+            {oldTransaction?.id ? "Atualizar transação" : "Adicionar transação"}
+          </Typo>
+        </Button>
+      </View>
+    </ModalWrapper>
+  );
+};
+
+export default TransactionModal;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingHorizontal: spacingY._20,
+  },
+  form: {
+    gap: spacingY._20,
+    paddingVertical: spacingY._15,
+    paddingBottom: spacingY._40,
+  },
+  footer: {
+    alignItems: "center",
+    flexDirection: "row",
+    justifyContent: "center",
+    paddingHorizontal: spacingX._20,
+    gap: scale(12),
+    paddingTop: spacingY._15,
+    borderTopColor: colors.neutral700,
+    marginBottom: spacingY._5,
+    borderTopWidth: 1,
+  },
+  inputContainer: {
+    gap: spacingY._10,
+  },
+  iosDropDown: {
+    flexDirection: "row",
+    height: verticalScale(54),
+    alignItems: "center",
+    justifyContent: "center",
+    fontSize: verticalScale(14),
+    borderWidth: 1,
+    color: colors.white,
+    borderRadius: radius._17,
+    borderCurve: "continuous",
+    paddingHorizontal: spacingX._15,
+  },
+  androidDropDown: {
+    // flexDirection: "row",
+    height: verticalScale(54),
+    alignItems: "center",
+    justifyContent: "center",
+    fontSize: verticalScale(14),
+    borderWidth: 1,
+    color: colors.white,
+    borderRadius: radius._17,
+    borderCurve: "continuous",
+    // paddingHorizontal: spacingX._15
+  },
+  flexRow: {
+    flexDirection: "row",
+    alignItems: "center",
+    gap: spacingX._5,
+  },
+  dateInput: {
+    flexDirection: "row",
+    height: verticalScale(54),
+    alignItems: "center",
+    borderWidth: 1,
+    borderColor: colors.neutral300,
+    borderRadius: radius._17,
+    borderCurve: "continuous",
+    paddingHorizontal: spacingX._15,
+  },
+  iosDatePicker: {
+    // backgroundColor: "rede"
+  },
+  datePickerButton: {
+    backgroundColor: colors.neutral700,
+    alignSelf: "flex-end",
+    padding: spacingY._7,
+    marginRight: spacingX._7,
+    paddingHorizontal: spacingY._15,
+    borderRadius: radius._10,
+  },
+  dropdownContainer: {
+    height: verticalScale(54),
+    borderWidth: 1,
+    borderColor: colors.neutral300,
+    paddingHorizontal: spacingX._15,
+    borderRadius: radius._15,
+    borderCurve: "continuous",
+  },
+  dropdownItemText: {
+    color: colors.white,
+  },
+  dropdownSelectedText: {
+    color: colors.white,
+    fontSize: verticalScale(14),
+  },
+  dropdownListContainer: {
+    backgroundColor: colors.neutral900,
+    borderRadius: radius._15,
+    borderCurve: "continuous",
+    paddingVertical: spacingY._7,
+    top: 5,
+    borderColor: colors.neutral500,
+    shadowColor: colors.black,
+    shadowOffset: {
+      width: 0,
+      height: 5,
+    },
+    shadowOpacity: 1,
+    shadowRadius: 15,
+    elevation: 5,
+  },
+  dropdownPlaceholder: {
+    color: colors.white,
+  },
+  dropdownItemContainer: {
+    borderRadius: radius._15,
+    marginHorizontal: spacingX._7,
+  },
+  dropdownIcon: {
+    height: verticalScale(30),
+    tintColor: colors.neutral300,
+  },
+});
